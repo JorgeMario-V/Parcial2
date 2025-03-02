12de002c79206b230920d700619a0d42b344d557
Your Name <you@example.com>
2025-03-02 02:07:58 -0500

    Primer commit

---
+++ src/main/java/com/example/apirestfulparcial/ApiRestfulParcialApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestfulparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestfulParcialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apirestfulparcial/ApiRestfulParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestfulparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulParcialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
